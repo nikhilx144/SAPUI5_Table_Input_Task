40ea86dd8b00794c54056dd61a56446885758365
Nikhil G <nikhilgelli1@gmail.com>
2026-06-05 12:47:02 +0000

    task halfway done. Need to add filters and update the selected option from the select dialog

--- webapp/controller/StudentTable.controller.js
+++ webapp/controller/StudentTable.controller.js
@@ -1,7 +1,10 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/core/Fragment"
-], (Controller, Fragment) => {
+    "sap/ui/core/Fragment",
+    "sap/ui/table/Column",
+    "sap/m/Label",
+    "sap/m/Text"
+], (Controller, Fragment, UIColumn, Label, Text) => {
     "use strict";
 
     return Controller.extend("input.in.table.tasks.ui5.ui5inputintablerowtask.controller.StudentTable", {
@@ -18,8 +21,6 @@ sap.ui.define([
         // on click of add row button, set newEntry prop with empty values and editableRow as true
         // push the newEntry to the Table prop in model and make the submit and cancel button visible
         onAddRow() {
-            // console.log(this.oModel);
-            // console.log(this.getView().getModel("stuDetails"));
             const stuData = this.oModel.getProperty("/Table");
             if (this.addRowCounter === 0) this.lastEntryIndex = stuData.length - 1;
             this.addRowCounter++;
@@ -75,42 +76,52 @@ sap.ui.define([
             this.lastEntryIndex = -1;
         },
 
-        onValueHelpRequested() {
-            const _this = this;
-            const view = this.getView();
-            if (!_this._oVHD) {
-                _this._oVHD = Fragment.load({
-                    name: "input.in.table.tasks.ui5.ui5inputintablerowtask.view.fragments.ValueHelpDialog",
-                }).
-                then(function (oDialog) {
-                    // this.oView.addDependent(oDialog);
-                    _this.getView().addDependent(oDialog);
-                    // view.addDependent(oDialog);
+        onSelectDialogForClgCodeRequested() {
+            if (!this._oSelectDialogForClgCode) {
+                this._oSelectDialogForClgCode = Fragment.load({
+                    name: "input.in.table.tasks.ui5.ui5inputintablerowtask.view.fragments.SelectDialogForClgCode",
+                    controller: this
+                }).then((oDialog) => {
+                    this.oView.addDependent(oDialog);
+                    oDialog.setModel(this.oModel);
                     return oDialog;
                 });
             }
-
-            this._oVHD.then(function (oDialog) {
+                
+            this._oSelectDialogForClgCode.then((oDialog) => {
                 oDialog.open();
             });
         },
 
-        onValueHelpDialogOk(event) {
-            const oSelectedItem = event.getParameter("selectedItems")[0];
-            if (oSelectedItem) {
-                const oInput = this.oView.byId("clgCodeInput");
-                oInput.setValue(oSelectedItem.getTitle());
+        // get the selected college code and update the value of the input field for which the value help dialog is opened with the selected college code
+        // show only the department codes in the value help dialog for department code which are relevant to the selected college code in the value help dialog for college code
+        onSelectDialogForClgCodeConfirm(event) {
+            // maybe store the selectedClgCode in this so that it can be used in the onSelectDialogForDeptCodeConfirm method to filter the department codes based on the selected college code
+            this.selectedClgCode = event.getParameter("selectedItems")[0].getTitle();
+        },
+
+        onSelectDialogForDeptCodeRequested() {
+            if (!this._oSelectDialogForDeptCode) {
+                this._oSelectDialogForDeptCode = Fragment.load({
+                    name: "input.in.table.tasks.ui5.ui5inputintablerowtask.view.fragments.SelectDialogForDeptCode",
+                    controller: this
+                }).then((oDialog) => {
+                    this.oView.addDependent(oDialog);
+                    oDialog.setModel(this.oModel);
+                    return oDialog;
+                });
             }
+                
+            this._oSelectDialogForDeptCode.then((oDialog) => {
+                oDialog.open();
+            });
         },
 
-        onValueHelpDialogCancel() {
-            const _this = this;
-            console.log(_this._oVHD);
-            _this._oVHD.close();
-        },
-
-        // onValueHelpDialogAfterClose() {
-        //     _this._oVHD.destroy();
-        // }
+        // get the selected department code and name from the event parameter and set the input field for which the value help dialog is opened with the selected department code
+        // and also set the text of the text tag for dept name with the corresponding department name of the selected dept code
+        onSelectDialogForDeptCodeConfirm(event) {
+            const selectedDeptCode = event.getParameter("selectedItems")[0].getTitle();
+            const selectedDeptName = event.getParameter("selectedItems")[0].getDescription();
+        }
     });
 });
